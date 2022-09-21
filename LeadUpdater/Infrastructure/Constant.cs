d322662cda353c40565ff8f8f08e7309ba2a9d97
alkanik <alkanik@gmail.com>
2022-09-21 22:16:12 +0300

    changed settings for release

--- LeadUpdater/Infrastructure/Constant.cs
+++ LeadUpdater/Infrastructure/Constant.cs
@@ -6,7 +6,6 @@ public class Constant
     public const string LeadStatisticsPath = "LeadStatistics";
     public const string LSTransactionPath = "/transactions-count?";
     public const string LSAmountPath = "/amount-difference?";
-    public const int TimeDelay = 5000;
     public const string CronExpressionTest = "0 * * ? * *";
-    public const string CronExpression = "0 0 23 ? * *"; //0 0 * ? * * *
+    public const string CronExpression = "0 0 * ? * *";
 }
